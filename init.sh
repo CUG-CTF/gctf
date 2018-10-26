a133e1ac1f10a408c67654f3161d2dc2e05c9256
CangShYao <1937636639@qq.com>
2018-10-26 18:40:08 +0800

    read this code and fix some

--- init.sh
+++ init.sh
@@ -1,7 +1,7 @@
 # You need get dep from github
 # May be need broke wall
-go get -u github.com/lib/pq
-go get -u github.com/docker/docker/client
-go get -u github.com/gin-gonic/gin
-go get -u github.com/go-xorm/xorm
-go get -u golang.org/x/crypto/bcrypt
+go get -v github.com/lib/pq
+go get -v github.com/docker/docker/client
+go get -v github.com/gin-gonic/gin
+go get -v github.com/go-xorm/xorm
+go get -v golang.org/x/crypto/bcrypt
--- server/model/data.go
+++ server/model/data.go
@@ -9,42 +9,49 @@ import (
 	"time"
 )
 
+// meaning GctfDataMange is a database
 var GctfDataManage *xorm.Engine
 
 func init() {
 	var err error
-
+	// go-xorm is used to create database engine
+	// engine, err := xorm.NewEngine(driverName, dataSourceName)
+	// data
 	GctfDataManage, err = xorm.NewEngine(gctfConfig.GCTF_DB_NAME, gctfConfig.GCTF_DB_STRING)
-	//All table name have gctf_ prefix
-	tbMapper:=core.NewPrefixMapper(core.GonicMapper{}, "gctf_")
+	// All table name have a gctf_ prefix
+	// prefix，前缀
+	tbMapper := core.NewPrefixMapper(core.GonicMapper{}, "gctf_")
 	// fix problem_I_D to problem_id
 	GctfDataManage.SetColumnMapper(core.GonicMapper{})
 	GctfDataManage.SetTableMapper(tbMapper)
 	if err != nil {
-		log.Fatal("database connet error:", err.Error())
+		log.Fatal("database connect error:", err.Error())
 	}
 }
 
 func init() {
-	err:=GctfDataManage.Ping()
-	if err!=nil{
-		log.Fatal("error",err.Error())
+	// Ping is test the database is alive
+	err := GctfDataManage.Ping()
+	if err != nil {
+		log.Fatal("error", err.Error())
 	}
-	if gctfConfig.GCTF_DEBUG{
+	if gctfConfig.GCTF_DEBUG {
 		GctfDataManage.ShowSQL(true)
 		GctfDataManage.Logger().SetLevel(core.LOG_DEBUG)
 
 	}
-	err=GctfDataManage.CreateTables(User{},Problems{},UserProblems{},Hints{},Tag{},Teams{})
-	//GctfDataManage.DropTables("gctf_user","gctf_problems","gctf_user_problems","gctf_hints","gctf_tag","gctf_teams")
+	// this is create lots of tables?
+	err = GctfDataManage.CreateTables(User{}, Problems{}, UserProblems{}, Hints{}, Tag{}, Teams{})
+	// GctfDataManage.DropTables("gctf_user","gctf_problems","gctf_user_problems","gctf_hints","gctf_tag","gctf_teams")
 	checkerr(err)
 }
 
-func checkerr(err error){
-	if err!=nil{
+func checkerr(err error) {
+	if err != nil {
 		log.Fatal(err)
 	}
 }
+
 type User struct {
 	Id             int64  `xorm:"autoincr pk 'id'"`
 	Username       string `xorm:"unique"`
@@ -55,30 +62,31 @@ type User struct {
 	SolvedProblems string
 	Score          int
 }
+
 type Problems struct {
 	Id          int64  `xorm:"autoincr pk 'id'"`
 	Name        string `xorm:"unique"`
-	Description string //Problem Description
-	Value       int    //score
+	Description string // Problem Description
+	Value       int    // score
 	Category    string
-	Hidden      bool                   //should be problem hide?
-	Location    string                 //saved physical position
-	Scale       int `xorm:"default 0"` //score scale when each answer submit
+	Hidden      bool   // should be problem hide?
+	Location    string // saved physical position
+	Scale       int    `xorm:"default 0"` // score scale when each answer submit
 }
 
 type UserProblems struct {
-	Id         int64 `xorm:"autoincr pk 'id'"`
-	UserId     int64  //foreignkey gctf_user.id
-	Location   string //problem net location
+	Id         int64  `xorm:"autoincr pk 'id'"`
+	UserId     int64  // foreignkey gctf_user.id
+	Location   string // problem net location
 	Flag       string
-	ProblemsId int64 //foreignkey gctf_problems.id
+	ProblemsId int64 // foreignkey gctf_problems.id
 }
 
 type Hints struct {
 	Id         int64 `xorm:"autoincr pk 'id'"`
-	ProblemsId int64 //foreignkey gctf_problems.id
+	ProblemsId int64 // foreignkey gctf_problems.id
 	Hint       string
-	Cost       int //cost score to get hint
+	Cost       int // cost score to get hint
 }
 
 type Tag struct {
@@ -91,5 +99,5 @@ type Teams struct {
 	Id     int64  `xorm:"autoincr pk 'id'"`
 	Name   string `xorm:"unique"`
 	Member string
-	Banned bool //if true this team can't login
+	Banned bool // if true this team can't login
 }
