ea7f2a605778d1ea62cbc17f3d45d6737d0f3523
zhihen.Wang <wzcboss@qq.com>
2019-03-04 07:14:17 +0800

    add untargz method

--- server/v1/admin.go
+++ server/v1/admin.go
@@ -2,8 +2,13 @@ package v1
 
 import (
 	"github.com/gin-gonic/gin"
+	"log"
+	"net/http"
+	"os"
+	"../gctfUtils"
 )
 
+//TODO: problems sync with database
 func AddUsers(c *gin.Context) {
 
 }
@@ -23,13 +28,41 @@ func ChangeGCTFMode(c *gin.Context) {
 }
 
 func UploadProblem(c *gin.Context) {
-	c.FormFile()
+	category:=c.PostForm("category")
+	description:=c.PostForm("description")
+	problem,err:=c.FormFile("problem")
+	if err!=nil{
+		log.Println("UploadProblem error:" +err.Error())
+		c.JSON(http.StatusBadRequest,gin.H{"msg":"upload file erroe"})
+		return
+	}
+	//Todo: write db, security check to vars
+	category=category
+	description=description
+
+	err=os.MkdirAll("problems/"+category,os.ModePerm)
+	if err !=nil{
+		c.JSON(http.StatusInternalServerError,gin.H{"msg":"error to create folder"+"problems/"+category,"err":err.Error()})
+	}
+	filename:=problem.Filename
+	//TODO: add zip support
+	err=c.SaveUploadedFile(problem,"problems/"+category+"/"+filename)
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError,gin.H{"msg":"error to save upload file","err":err.Error()})
+	}
+	f,err:=os.Open("problems/"+category+"/"+filename)
+	if err!=nil{
+		c.JSON(http.StatusInternalServerError,gin.H{"msg":"error to read upload file","err":err.Error()})
+	}
+	gctfUtils.ExtractTarGz(f)
 
 }
+//change a problem's information
 func ChangeProblem(c *gin.Context) {
 
 }
 
+//delete a problem from disk and db
 func DeleteProblem(c *gin.Context) {
 
 }
--- server/v1/problem.go
+++ server/v1/problem.go
@@ -1,6 +1,8 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func StartProblem( c *gin.Context){
 
