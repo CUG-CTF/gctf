055ff3f53fd0f2fadfbe3dfba3e929cdbebfc049
windforce17 <wzcboss@qq.com>
2019-06-03 19:38:35 +0800

    [gctf/cmd] add command line tool

--- cmd/gctf_tool.go
+++ cmd/gctf_tool.go
@@ -1,6 +1,23 @@
 package main
 
-//todo check,backup,restory,upload
+import (
+	"fmt"
+	"log"
+	"github.com/urfave/cli"
+	"os"
+)
+
+//todo check,backup,restore,upload
 func main() {
+	app:=cli.NewApp()
+	app.Name = "gctf-tool"
+	app.Usage="to check backup restore upload problem"
+	app.Action= func(context *cli.Context) {
+		fmt.Println("666")
+	}
+	err:=app.Run(os.Args)
+	if err!=nil	{
+		log.Fatal(err)
+	}
 
 }
