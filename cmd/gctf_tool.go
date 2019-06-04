3bf2dfee5d642861b877d5b28d69dae95c87f322
unknown <wzcboss@qq.com>
2019-06-04 19:47:22 +0800

    try to use cli framwork

--- cmd/gctf_tool.go
+++ cmd/gctf_tool.go
@@ -2,21 +2,32 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"github.com/urfave/cli"
+	"log"
 	"os"
 )
 
 //todo check,backup,restore,upload
 func main() {
-	app:=cli.NewApp()
+	app := cli.NewApp()
 	app.Name = "gctf-tool"
-	app.Usage="to check backup restore upload problem"
-	app.Action= func(context *cli.Context) {
-		fmt.Println("666")
+	app.Version = "0.01"
+	app.Usage = "to check backup restore upload problem"
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "conf,c",
+			Value: "conf.json",
+			Usage: "config file path",
+		},
 	}
-	err:=app.Run(os.Args)
-	if err!=nil	{
+
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("666")
+		fmt.Println(c.String("c"))
+		return nil
+	}
+	err := app.Run(os.Args)
+	if err != nil {
 		log.Fatal(err)
 	}
 
